8c3299d8ab9701daf383fa2b4e336c4364f64a23
yangxin <1393003255@qq.com>
2025-08-04 20:36:11 +0800

    feat: 服务发现 - nacos ，为内容中心引入服务发现

--- content-center/src/main/java/org/yangxin/contentcenter/service/content/ShareService.java
+++ content-center/src/main/java/org/yangxin/contentcenter/service/content/ShareService.java
@@ -1,7 +1,10 @@
 package org.yangxin.contentcenter.service.content;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.yangxin.contentcenter.dao.content.ShareMapper;
@@ -9,14 +12,16 @@ import org.yangxin.contentcenter.domain.dto.content.ShareDTO;
 import org.yangxin.contentcenter.domain.dto.user.UserDTO;
 import org.yangxin.contentcenter.domain.entity.content.Share;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ShareService {
-
     private final ShareMapper shareMapper;
     private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
 
     public ShareDTO findById(Integer id) {
         Share share = this.shareMapper.selectByPrimaryKey(id);
@@ -25,7 +30,14 @@ public class ShareService {
         }
         Integer userId = share.getUserId();
 
-        UserDTO userDTO = this.restTemplate.getForObject("http://localhost:8080/users/{id}", UserDTO.class, userId);
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("user-center");
+        String targetURL = instanceList.stream()
+                .map(tmp -> tmp.getUri().toString() + "/users/{id}")
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前没有实例"));
+        log.info("targetURL:{}", targetURL);
+
+        UserDTO userDTO = this.restTemplate.getForObject(targetURL, UserDTO.class, userId);
         if (Objects.isNull(userDTO)) {
             return null;
         }
