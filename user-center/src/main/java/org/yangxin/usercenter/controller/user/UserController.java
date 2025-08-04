aa950318e17627be38ca7d8fdb0dbb3f03c1eb44
yangxin <1393003255@qq.com>
2025-08-04 21:59:54 +0800

    feat: 实现负载均衡 - ribbon ， ribbon 内置的负载均衡规则

--- user-center/src/main/java/org/yangxin/usercenter/controller/user/UserController.java
+++ user-center/src/main/java/org/yangxin/usercenter/controller/user/UserController.java
@@ -1,6 +1,7 @@
 package org.yangxin.usercenter.controller.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,14 @@ import org.yangxin.usercenter.service.user.UserService;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{id}")
     public User findById(@PathVariable Integer id) {
+        log.info("我被请求了");
         return this.userService.findById(id);
     }
 }
