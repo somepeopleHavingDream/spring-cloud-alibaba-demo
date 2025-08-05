cf93ccf3043564ebe520cb8be6063471fdf1f201
yangxin <1393003255@qq.com>
2025-08-05 23:09:31 +0800

    feat: 实现负载均衡 - ribbon ，细粒度配置自定义 02- 父子上下文

--- content-center/src/main/java/org/yangxin/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/org/yangxin/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -2,7 +2,7 @@ package org.yangxin.contentcenter.configuration;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
-import ribbonconfiguration.RibbonConfiguration;
+import org.yangxin.ribbonconfiguration.RibbonConfiguration;
 
 @RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
 @Configuration
--- content-center/src/main/java/org/yangxin/ribbonconfiguration/RibbonConfiguration.java
+++ content-center/src/main/java/org/yangxin/ribbonconfiguration/RibbonConfiguration.java
@@ -1,4 +1,4 @@
-package ribbonconfiguration;
+package org.yangxin.ribbonconfiguration;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
