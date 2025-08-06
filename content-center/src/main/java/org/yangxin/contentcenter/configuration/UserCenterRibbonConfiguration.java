7a5491c0510899da74ff2bf86a9db919cbdeb44f
yangxin <1393003255@qq.com>
2025-08-06 20:13:28 +0800

    feat: 实现负载均衡 - ribbon ，全局配置

--- content-center/src/main/java/org/yangxin/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/org/yangxin/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,10 +1,10 @@
-//package org.yangxin.contentcenter.configuration;
-//
-//import org.springframework.cloud.netflix.ribbon.RibbonClient;
-//import org.springframework.context.annotation.Configuration;
-//import org.yangxin.ribbonconfiguration.RibbonConfiguration;
-//
-//@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
-//@Configuration
-//public class UserCenterRibbonConfiguration {
-//}
+package org.yangxin.contentcenter.configuration;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.context.annotation.Configuration;
+import org.yangxin.ribbonconfiguration.RibbonConfiguration;
+
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
+@Configuration
+public class UserCenterRibbonConfiguration {
+}
