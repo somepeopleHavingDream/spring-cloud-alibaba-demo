171f4bccdcf2bd464da2fb81b45d021b8a3d2e1b
yangxin <1393003255@qq.com>
2025-08-19 20:44:56 +0800

    feat: API 网关 -Spring Cloud Gateway ，自定义路由谓词工厂

---
+++ gateway/src/main/java/com/yangxin/gateway/TimeBetweenRoutePredicateFactory.java
@@ -0,0 +1,35 @@
+package com.yangxin.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+@Slf4j
+public class TimeBetweenRoutePredicateFactory extends AbstractRoutePredicateFactory<TimeBetweenConfig> {
+    public TimeBetweenRoutePredicateFactory() {
+        super(TimeBetweenConfig.class);
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(TimeBetweenConfig config) {
+        LocalTime start = config.getStart();
+        LocalTime end = config.getEnd();
+        return exchange -> {
+            LocalTime now = LocalTime.now();
+            log.info("apply {}", now);
+            return now.isAfter(start) && now.isBefore(end);
+        };
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("start", "end");
+    }
+}
