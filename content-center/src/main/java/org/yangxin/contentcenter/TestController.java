0c3d963f811be996ccbcd649ea4755153c2a0d7c
yangxin <1393003255@qq.com>
2025-08-21 20:24:18 +0800

    feat: 配置管理 -Nacos ，配置属性动态刷新与回滚

--- content-center/src/main/java/org/yangxin/contentcenter/TestController.java
+++ content-center/src/main/java/org/yangxin/contentcenter/TestController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.yangxin.contentcenter.dao.content.ShareMapper;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RefreshScope
 public class TestController {
     @Resource
     private ShareMapper shareMapper;
